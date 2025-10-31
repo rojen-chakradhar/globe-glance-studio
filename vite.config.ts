98d2c4d28a372e73b79a02b6ae332fa1f3cbb0fe
Rojen Chakradhar <rojenchakradhar72@gmail.com>
2025-10-31 23:35:24 +0545

    hope it works

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
     port: 8080,
     allowedHosts: [
-      "77c2f42c-c197-46dc-b106-c451c8c87ae2-00-qpk8u6rxfjmp.riker.replit.dev",
+      "77c2f42c-c197-46dc-b106-c451c8c87ae2-00-qpk8u6rxfjmp.riker.replit.dev","::",
     ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
